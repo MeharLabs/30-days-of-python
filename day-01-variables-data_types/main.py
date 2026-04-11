a289b1b9be9ef5f1ec578c0e050ea5ffb39bf49c
Mehar Jawad Hashmi <jawadhashmi.info@gmail.com>
2026-04-11 14:52:23 +0500

    learnt about the data types

--- day-01-variables-data_types/main.py
+++ day-01-variables-data_types/main.py
@@ -1,7 +1,46 @@
 # ============================================
 # 🐍 Day 01 — Variables & Data Types
-# 📅 Date: DD/MM/YYYY
-# 🎯 Goal: What you're practicing today
+# 📅 Date: 11/04/2026
+# 🎯 Goal: Learn how to save data to variables and vibe with different data types so your code actually remembers stuff and doesn't just flop. 💀🐍
 # ============================================
 
-# --- Your code starts here ---
+# --- code starts from here ---
+
+# Data Types
+
+# int
+age = 24
+number = 5
+
+# float
+price = 19.99
+quantity = 29.09
+
+# str
+greeting = "Hi"
+name = "Red John"
+
+# str - blank
+name = ""
+
+# str - Empty Space
+name = " "
+
+# bool
+online = True
+online = False
+
+# list
+tasks = ["wake up", "eat", "code", "sleep"]
+marks = [85, 90, 78, 92]
+
+# tuple
+days = ("Mon", "Tue", "Wed", "Thu", "Fri")
+red = (255, 0, 0)
+
+# dict
+user = {
+    "name": "John",
+    "age": 24,
+    "vibe": "coding"
+}
